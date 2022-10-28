4e7179f3b464230284ca05ca36d786a47c782c69
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2022-10-28 23:30:01 +0800

    Menambahkan fungsi login, regist, & logout.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,9 +9,25 @@ class HomeController extends Controller
     //
     public function index()
     {
-        return view('guest.pages.index',[
+        return view('guest.pages.index', [
             'title' => 'Home',
             'active' => 'home'
+        ]);
+    }
+
+    public function staff()
+    {
+        return view('staff.pages.index', [
+            'title' => 'Staff Home',
+            'active' => 'staff.home'
+        ]);
+    }
+
+    public function villa()
+    {
+        return view('villa.pages.index', [
+            'title' => 'Villa',
+            'active' => 'villa'
         ]);
     }
 }
