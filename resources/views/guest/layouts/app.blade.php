4776e730a0613e403aa5642730653c28c2e10ff0
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2022-10-28 18:06:36 +0800

    Admin tamplate

---
+++ resources/views/guest/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Staff Seekers | </title>
+    @include('guest.layouts._css')
+</head>
+
+<body>
+    @include('guest.layouts._nav')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('guest.layouts._js')
+</body>
+
+</html>
