bb3fbfc74c99baaa6939fe98da001bb102af5a3a
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2022-10-29 11:50:26 +0800

    Menambah layout

--- resources/views/villa/pages/index.blade.php
+++ resources/views/villa/pages/index.blade.php
@@ -1,1 +1,13 @@
-villa
+@extends('staff.layouts.app')
+
+@section('content')
+    {{-- Intro Section Start --}}
+    @include('components.introSection')
+    {{-- Intro Section End --}}
+
+    <section>
+        <h1 class="text-center">
+            Villa
+        </h1>
+    </section>
+@endsection
