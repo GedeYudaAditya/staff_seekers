038bf22fa5b41ffb993b451e77ca6dfb4caff7ca
wahyusinggihw <wahyusinggih2402@gmail.com>
2022-11-02 00:59:44 +0800

    add card to findstaff villa

--- resources/views/villa/pages/findstaff.blade.php
+++ resources/views/villa/pages/findstaff.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 {{-- Intro Section Start --}}
 <div class="row justify-content-center mt-5">
-
     <div class="col-lg-4 px-3">
         <div class="text-center">
             <h5 class="card-title">Special title treatment</h5>
@@ -11,8 +10,28 @@
             <a href="#" class="btn btn-danger">Go somewhere</a>
         </div>
     </div>
-
-
 </div>
 
+<div class="row mt-4">
+    {{-- TINGGAL AKTIFIN AJA KALO MAU DIPAKEK --}}
+    {{-- @foreach ($staffs as $staff) --}}
+    <div class="col-md-4 mb-3">
+        <a href="#">
+            <div class="card mb-3">
+                {{-- @if ($staff['foto'] != null) --}}
+                {{-- <div style="max-height: 400px; overflow: hidden;">
+                    <img src="{{ $staff['foto'] }}" class="card-img-top img-fluid" alt="{{ $guide->image }}">
+                </div> --}}
+                {{-- @else --}}
+                <img src="https://reqres.in/img/faces/7-image.jpg" class="card-img-top img-fluid" alt="user-example">
+                {{-- @endif --}}
+        </a>
+        <div class="card-body">
+            <h5 class="card-title" style="text-decoration: none;">Nama</h5>
+            <p class="card-text">Deskripsi</p>
+        </div>
+    </div>
+</div>
+{{-- @endforeach --}}
+</div>
 @endsection
