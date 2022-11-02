3dba0bce2ea1a907a29a07fa6b585912a57a5ff0
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2022-11-02 09:48:06 +0800

    baru

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,9 @@ class UserFactory extends Factory
             'username' => fake()->userName(),
             'role' => fake()->randomElement(['admin', 'staff', 'villa']),
             'status' => fake()->randomElement(['active', 'inactive']),
+            'bio' => fake()->sentence(),
+            'detailBio' => fake()->paragraph(),
+            'image' => fake()->imageUrl(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- resources/views/villa/pages/findstaff.blade.php
+++ resources/views/villa/pages/findstaff.blade.php
@@ -1,37 +1,41 @@
 @extends('villa.layouts.app')
 
 @section('content')
-{{-- Intro Section Start --}}
-<div class="row justify-content-center mt-5">
-    <div class="col-lg-4 px-3">
-        <div class="text-center">
-            <h5 class="card-title">Special title treatment</h5>
-            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-danger">Go somewhere</a>
+    {{-- Intro Section Start --}}
+    <div class="row justify-content-center mt-5">
+        <div class="col-lg-4 px-3">
+            <div class="text-center">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-danger">Go somewhere</a>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="row mt-4">
-    {{-- TINGGAL AKTIFIN AJA KALO MAU DIPAKEK --}}
-    {{-- @foreach ($staffs as $staff) --}}
-    <div class="col-md-4 mb-3">
-        <a href="#">
-            <div class="card mb-3">
-                {{-- @if ($staff['foto'] != null) --}}
-                {{-- <div style="max-height: 400px; overflow: hidden;">
-                    <img src="{{ $staff['foto'] }}" class="card-img-top img-fluid" alt="{{ $guide->image }}">
-                </div> --}}
-                {{-- @else --}}
-                <img src="https://reqres.in/img/faces/7-image.jpg" class="card-img-top img-fluid" alt="user-example">
-                {{-- @endif --}}
-        </a>
-        <div class="card-body">
-            <h5 class="card-title" style="text-decoration: none;">Nama</h5>
-            <p class="card-text">Deskripsi</p>
+    <div class="row mt-4">
+        {{-- TINGGAL AKTIFIN AJA KALO MAU DIPAKEK --}}
+        <div class="row row-cols-1 row-cols-md-3 g-4 mb-5 mx-0">
+            @foreach ($staffs as $staff)
+                <div class="col">
+                    <div class="card h-100">
+                        @if ($staff['image'] != null)
+                            <img style="height: 200px" src="{{ $staff['image'] }}" class="card-img-top img-fluid"
+                                alt="{{ $staff->image }}">
+                        @else
+                            <img style="height: 200px" src="https://reqres.in/img/faces/7-image.jpg"
+                                class="card-img-top img-fluid" alt="user-example">
+                        @endif
+                        <div class="card-body">
+                            <div class="card-body">
+                                <h5 class="card-title" style="text-decoration: none;">Nama: {{ $staff->name }}</h5>
+                                <p class="card-text"><b>Bio: </b> <br>
+                                    {{ $staff->bio }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
-</div>
-{{-- @endforeach --}}
-</div>
 @endsection
